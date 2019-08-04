20af2b234985c64c0ee52ad86754d09a87034232
bitsurgeon <43927708+bitsurgeon@users.noreply.github.com>
2019-08-04 14:32:12 +1000

    feat: implement ParticleFilter::init()

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -20,6 +20,7 @@
 
 using std::string;
 using std::vector;
+using std::normal_distribution;
 
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
   /**
@@ -30,8 +31,26 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
    * NOTE: Consult particle_filter.h for more information about this method 
    *   (and others in this file).
    */
-  num_particles = 0;  // TODO: Set the number of particles
+  num_particles = 100;  // TODO: Set the number of particles
 
+  std::default_random_engine gen;
+  normal_distribution<double> dist_x(x, std[0]);
+  normal_distribution<double> dist_y(y, std[1]);
+  normal_distribution<double> dist_theta(theta, std[2]);
+
+  for (int i = 0; i < num_particles; ++i) {
+    Particle aParticle;
+    aParticle.id = i;
+    aParticle.x = dist_x(gen);
+    aParticle.y = dist_y(gen);
+    aParticle.theta = dist_theta(gen);
+    aParticle.weight = 1.0;
+
+    particles.push_back(aParticle);
+    weights.push_back(1.0);
+  }
+
+  is_initialized = true;
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], 
