5c9899d7caa80f44ed7119242d9e274d832bf309
bitsurgeon <43927708+bitsurgeon@users.noreply.github.com>
2019-08-05 22:06:36 +1000

    refactor: remove unused member function declaration

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -83,19 +83,6 @@ void ParticleFilter::prediction(double delta_t, double std_pos[],
   }
 }
 
-void ParticleFilter::dataAssociation(vector<LandmarkObs> predicted, 
-                                     vector<LandmarkObs>& observations) {
-  /**
-   * TODO: Find the predicted measurement that is closest to each 
-   *   observed measurement and assign the observed measurement to this 
-   *   particular landmark.
-   * NOTE: this method will NOT be called by the grading code. But you will 
-   *   probably find it useful to implement this method and use it as a helper 
-   *   during the updateWeights phase.
-   */
-
-}
-
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], 
                                    const vector<LandmarkObs> &observations, 
                                    const Map &map_landmarks) {
